2dfc71eacfe6f6b88fd120fb7e362344bbcc4805
Tanja Santner <tsantner.mmt-b2020@fh-salzburg.ac.at>
2022-03-17 15:14:58 +0100

    Signin Form doesn't work correctly yet, input is empty (console.log)

--- src/components/UserSignIn.js
+++ src/components/UserSignIn.js
@@ -2,20 +2,26 @@ import React from 'react'
 import { Button } from './Button'
 import { TextField } from './TextField'
 import { Link } from './Link'
+import { useFormik } from 'formik'
 import styles from './Basics.module.css'
 
 export const UserSignIn = () => {
-  const submitAlert = (e) => {
-    e.preventDefault()
-    alert("Button doesn't work.")
-  }
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    onSubmit: values => {
+      console.log(values)
+    }
+  })
 
   return (
     <div className={`${styles.wrapper}`}>
-      <form onSubmit={submitAlert}>
-        <TextField label="Email" type="email"></TextField>
-        <TextField label="Password" type="password"></TextField>
-        <Button onClick={submitAlert}>Sign In</Button>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField onChange={formik.handleChange} value={formik.values.email} label="Email" type="email"></TextField>
+        <TextField onChange={formik.handleChange} value={formik.values.password} label="Password" type="password"></TextField>
+        <Button>Sign In</Button>
         <Link link="#" name="Sign Up"></Link>
       </form>
     </div>
