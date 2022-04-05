2e90271d3868989f4c6b524835881242644f584a
Tanja Santner <tsantner.mmt-b2020@fh-salzburg.ac.at>
2022-04-05 15:37:25 +0200

    Task 1 done, Task 2 almost done (TO-DO: paid button)

--- src/components/MoneyTransactionCreate.js
+++ src/components/MoneyTransactionCreate.js
@@ -4,46 +4,24 @@ import { DecimalInput } from './DecimalInput'
 import { Button } from './Button'
 import { useFormik } from 'formik'
 import styles from './MoneyTransactionCreate.module.css'
-import { db } from '../firebase-config.js'
-import { collection, getDocs, setDocs, doc } from 'firebase/firestore'
-import { findAllByTestId } from '@testing-library/react'
 
-export const MoneyTransactionCreate = ({ users, setTransactions }) => {
+export const MoneyTransactionCreate = ({ users = [], onSubmit }) => {
   const ownID = 'ejhHpqUKifetuDBvQlRK'
-  const newTransactionRef = doc(collection(db, 'transactions'))
-  const transactionCollectionRef = collection(db, 'transactions')
 
-  const formik = useFormik()({
+  const formik = useFormik({
     initialValues: {
-      debitorid: 0,
-      creditorId: 1,
+      debitorId: '',
+      creditorId: ownID,
       amount: 0
     },
-    onSubmit={ async (values, { setSubmitting }) => {
-      const transaction = { amount: values.amount, paidAt: null }
-      onSubmit(transaction.debitorid = ownID, transaction.creditorId = values.userId)
-
-      await setDocs(newTransactionRef, transaction)
-      setTransactions(await getTransactions())
-    }}
+    onSubmit: values => {
+      onSubmit(values.debitor, values.creditorId, values.amount)
+    }
   })
 
-
-  onSubmit={ async (values, { setSubmitting }) => {
-    await ...
-    setSubmitting(false)
-}}
-
-
-  const getTransactions = async () => {
-    const data = await getDocs(transactionCollectionRef)
-    const parsedData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-    return parsedData
-  }
-
   return (
-    <form className={`${styles.wrapper}`} onSubmit={formik.handleSubmit} setTransaction={setTransactions}>
-      <Dropdown onChange={formik.handleChange} value={formik.values.debitorid} options={users} label='DebitorId' className={`${styles.column1}`} />
+    <form className={`${styles.wrapper}`} onSubmit={formik.handleSubmit}>
+      <Dropdown onChange={formik.handleChange} options={users} label='Debitor' className={`${styles.column1}`} />
       <DecimalInput onChange={formik.handleChange} amount={formik.values.amount} label="Amount" />
       <Button style='secondary' className={`${styles.column3}`}>Create</Button>
     </form>
--- src/components/MoneyTransactionPage.js
+++ src/components/MoneyTransactionPage.js
@@ -2,61 +2,41 @@ import React, { useEffect, useState } from 'react'
 import { MoneyTransactionCreate } from './MoneyTransactionCreate'
 import { MoneyTransactionList } from './MoneyTransactionList'
 import { db } from '../firebase-config.js'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, doc, setDoc, getDocs } from 'firebase/firestore'
 
 export const MoneyTransactionPage = () => {
   const userCollectionRef = collection(db, 'users')
   const transactionCollectionRef = collection(db, 'transactions')
-
-  // const [moneyTransactions, setTransactions] = useState([])
-  // const [users, setUsers] = useState([])
-
-  // useEffect(() => {
-  //   fetch('http://localhost:3001/money-transaction')
-  //     .then((response) => response.json())
-  //     .then((json) => setTransactions(json))
-  //   fetch('http://localhost:3001/user')
-  //     .then((response) => response.json())
-  //     .then((json) => setUsers(json))
-  // }, [])
-
-  // async function handleSubmit(debitor, creditor, amount) {
-  //   await fetch('http://localhost:3001/money-transaction', {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/json' },
-  //     body: JSON.stringify({ debitorId: parseInt(debitor, 10), creditorId: creditor, amount: amount, paidAt: null })
-  //   })
-  //   await fetch('http://localhost:3001/money-transaction')
-  //     .then((response) => response.json())
-  //     .then((json) => setTransactions(json))
-  // }
-
   const [users, setUsers] = useState([])
   const [transactions, setTransactions] = useState([])
 
   useEffect(() => {
-    setUsers(getUsers())
-    setTransactions(getTransaction())
+    getUsers()
+    getTransaction()
   }, [])
+
+  async function handleSubmit (debitor, creditor, amount) {
+    const newTransaction = doc(collection(db, 'transactions'))
+    await setDoc(newTransaction, { creditorId: creditor, debitorId: debitor, amount: amount, paidAt: null })
+    getTransaction()
+  }
 
   async function getUsers () {
     const data = await getDocs(userCollectionRef)
-
-    // We generate our own user objects which match our expected schema
     const parsedData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
     console.log(parsedData)
+    setUsers(parsedData)
   }
   async function getTransaction () {
     const data = await getDocs(transactionCollectionRef)
-
-    // We generate our own user objects which match our expected schema
     const parsedData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
     console.log(parsedData)
+    setTransactions(parsedData)
   }
 
   return (
     <>
-      <MoneyTransactionCreate users={users} setTransactions={transactions} />
+      <MoneyTransactionCreate users={users} onSubmit={handleSubmit} />
       <MoneyTransactionList transaction={transactions} users={users} />
     </>
   )
